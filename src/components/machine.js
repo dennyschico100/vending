d477cb8a67cf97c1ae4088e00a76c90d1175d077
Dennys Cordova <dennyschico2@gmail.com>
2022-01-06 16:07:40 -0600

    feat:added Vending component and bootstrap

---
+++ src/components/machine.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState, useReducer, useRef } from 'react';
+const Vending = () => {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <h1 className="text-center">BIENVENIDO HAZ TU COMPRA AQUI</h1>
+      </div>
+    </React.Fragment>
+  );
+};
+export default Vending;
