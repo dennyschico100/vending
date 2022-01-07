e22a7fe4b0d1786bf38a752f0ee708cf34b6b8c5
Dennys Cordova <dennyschico2@gmail.com>
2022-01-07 00:19:14 -0600

    feat:added conditional rendering a loading effect

--- src/components/machine.js
+++ src/components/machine.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState, useReducer, useRef } from 'react';
 const Vending = () => {
   const API_URL = 'https://vending-machine-test.vercel.app/api/products';
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [totalProducts, setTotalProducts] = useState(0);
   const getProdducts = async () => {
     try {
       const res = await fetch(API_URL);
@@ -9,6 +11,11 @@ const Vending = () => {
       console.log(obj);
       console.log(obj.data);
       setProducts(obj.data);
+      setTotalProducts(products.length);
+      console.log(totalProducts);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2500);
     } catch (error) {
       throw Error(error);
     }
@@ -23,88 +30,34 @@ const Vending = () => {
         <div className="row">
           <div id="col-container" className=" col-md-8 border ">
             <div className="text-center d-flex justify-content-center flex-wrap ">
-              {products.map((products) => {
-                const { id, thumbnail, preparation_time } = products;
-                return (
-                  <ul
-                    key={id}
-                    className="col-md-4 list-unstyled  card-header text-center"
-                  >
-                    <li className="card-header">
-                      <strong>ID: </strong> {id}
-                    </li>
-                    <li className="card card-body">
-                      <img
-                        src={thumbnail}
-                        alt={thumbnail}
-                        className="img-fluid"
-                      />
-                    </li>
-                    <li className="card card-body">
-                      <strong>TIEMPO: </strong>
-                      {preparation_time}
-                    </li>
-                  </ul>
-                );
-              })}
+              {isLoading ? (
+                <LoadingComponent></LoadingComponent>
+              ) : (
+                products.map((product) => {
+                  return (
+                    <ShowProducts
+                      key={product.id}
+                      product={product}
+                    ></ShowProducts>
+                  );
+                })
+              )}
             </div>
           </div>
           <div className="border col-md-4">
-            <h2 className="  text-center mt-5 ">Selecciona</h2>
+            <h2 id="head-wrapper-menu" className="  text-center mt-5 ">
+              Selecciona
+            </h2>
             <div id="wrapper-menu">
               <ul className="panels-menu d-flex justify-content-center flex-wrap">
-                <li className="col-md-4">
-                  <div className="foor-number"></div>
-                  <p className="btn btn-secondary"> 1</p>
-                </li>
-                <li className="col-md-4">
-                  <div className="foor-number"></div>
-                  <p className="btn btn-secondary">2</p>
-                </li>
-                <li className="col-md-4">
-                  <div className="foor-number"></div>
-                  <p className="btn btn-secondary">3</p>
-                </li>
-                <li className="col-md-4">
-                  <div className="foor-number"></div>
-                  <p className="btn btn-secondary">4</p>
-                </li>
-                <li className="col-md-4">
-                  <div className="foor-number"></div>
-                  <p className="btn btn-secondary">5</p>
-                </li>
-                <li className="col-md-4">
-                  <div className="foor-number"></div>
-                  <p className="btn btn-secondary">6</p>
-                </li>
-                <li className="col-md-4">
-                  <div className="foor-number"></div>
-                  <p className="btn btn-secondary">7</p>
-                </li>
-                <li className="col-md-4">
-                  <div className="foor-number"></div>
-                  <p className="btn btn-secondary">8</p>
-                </li>
-                <li className="col-md-4">
-                  <div className="foor-number"></div>
-                  <p className="btn btn-secondary">9</p>
-                </li>
-                <li className="col-md-4">
-                  <div className="foor-number"></div>
-                  <p className="btn btn-secondary">10</p>
-                </li>
-                <li className="col-md-4">
-                  <div className="foor-number"></div>
-                  <p className="btn btn-secondary">11</p>
-                </li>
-                <li className="col-md-4">
-                  <div className="foor-number"></div>
-                  <p className="btn btn-secondary">12</p>
-                </li>
-                <li className="col-md-4">
-                  <div className="foor-number"></div>
-                  <p className="btn btn-secondary">13</p>
-                </li>
+                {products.map((product, index) => {
+                  return (
+                    <li key={product.id} className="col-md-4">
+                      <div className="foor-number"></div>
+                      <p className="btn btn-secondary">{index + 1}</p>
+                    </li>
+                  );
+                })}
               </ul>
               <div id="botones-container">
                 <button id="comprar" className="btn btn-success">
@@ -128,6 +81,30 @@ const Vending = () => {
         </div>
       </div>
     </React.Fragment>
+  );
+};
+const ShowProducts = (props) => {
+  const { id, thumbnail, preparation_time } = props.product;
+  return (
+    <ul className="col-md-4 list-unstyled  card-header text-center">
+      <li className="card card-body">
+        <strong>ID: </strong> {id}
+        <img src={thumbnail} alt={thumbnail} className="img-fluid" />
+      </li>
+      <li className="card card-body">
+        <strong>TIEMPO: </strong>
+        {preparation_time}
+      </li>
+    </ul>
+  );
+};
+const LoadingComponent = () => {
+  return (
+    <div className="lds-div">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   );
 };
 export default Vending;
